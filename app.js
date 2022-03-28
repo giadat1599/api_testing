f99a9aee0b8714a874bbb9b340c1591161f186c4
giadat99 <truonggiadat1999@gmail.com>
2022-03-28 09:24:41 +0700

    fix

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 const express = require('express');
 const connectMongoDB = require('./config/database');
 const useErrorHandler = require('./middlewares/error');
+const cors = require('cors');
 
 //Routes;
 const userRouter = require('./routes/user');
@@ -10,6 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/auth', userRouter);
 app.use(useErrorHandler);
